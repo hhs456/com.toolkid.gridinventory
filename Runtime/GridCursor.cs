7396845fc99e54e872367ea679f0b8754c34c774
Hanson <75490747+hhs456@users.noreply.github.com>
2025-02-24 20:47:50 +0800

    feat(grid): create `class: GridCursor` to detect which grid was clicked

---
+++ Runtime/GridCursor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+
+namespace Toolkid.UIGrid {
+    /// <summary>
+    /// Manages grid-based calculations and transitions using Unity's Grid system.
+    /// </summary>
+    public class GridCursor : MonoBehaviour {
+        [SerializeField] private GridRegion benchmark;
+        public event EventHandler<Vector2Int> Clicked;
+        private void Update() {
+            if (Input.GetMouseButtonUp(0)) {
+                RectTransformUtility.ScreenPointToWorldPointInRectangle(benchmark.GetComponent<RectTransform>(), Input.mousePosition, Camera.main, out Vector3 cursorPos);
+                Vector2Int gridIndex = benchmark.GetIndex(cursorPos);
+                Clicked?.Invoke(this, gridIndex);                
+            }
+        }
+    }
+}
